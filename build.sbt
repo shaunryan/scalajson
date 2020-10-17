517a973e853e9e137b27da0b183dbcc998fb4540
Shaun Ryan <shaun.ryan@adarga.ai>
2020-10-17 14:02:53 +0100

    commence gson

--- build.sbt
+++ build.sbt
@@ -7,3 +7,6 @@ scalaVersion := "2.13.3"
 
 // https://mvnrepository.com/artifact/org.sharegov/mjson
 libraryDependencies += "org.sharegov" % "mjson" % "1.4.1"
+
+// https://mvnrepository.com/artifact/com.google.code.gson/gson
+libraryDependencies += "com.google.code.gson" % "gson" % "2.8.6"
---
+++ src/main/scala/com/sibytes/gson/IntroToGson.scala
@@ -0,0 +1,21 @@
+package com.sibytes.gson
+
+import java.text.DateFormat
+
+import com.google.gson.{FieldNamingPolicy, Gson, GsonBuilder}
+import com.google.gson.internal.GsonBuildConfig
+
+object IntroToGson extends App{
+
+  val gson1 = new Gson()
+  val gson2 = new GsonBuilder()
+//    .registerTypeAdapter(Id.class, new IdTypeAdapter())
+    .serializeNulls() // by default nulls are skipped.
+    .setDateFormat(DateFormat.LONG)
+    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+    .setPrettyPrinting()
+    .setVersion(1.0)
+    .create()
+
+
+}
